bce9098ef76076468eea558e95a8b99c40c81e8a
Andre <meirezende@hotmail.com>
2022-02-10 15:32:46 -0300

    Changed CI/CD release

--- .github/scripts/new_version.sh
+++
@@ -1,6 +0,0 @@
- [[ $(grep --count "{version}" CHANGELOG.md) -ne 1 ]] && echo "Placeholder {version} not defined in CHANGELOG.md" && exit 1
- [[ $(grep --count "{date}" CHANGELOG.md) -ne 1 ]] && echo "Placeholder {date} not defined in CHANGELOG.md" && exit 1
- [[ $(grep --fixed-strings --count "# $VERSION" CHANGELOG.md) -ne 0 ]] && echo "Version $VERSION already defined in CHANGELOG.md" && exit 1
-
- sed -i 's/{version}/'"$VERSION"'/' CHANGELOG.md
- sed -i 's!{date}!'"$DATE"'!' CHANGELOG.md
