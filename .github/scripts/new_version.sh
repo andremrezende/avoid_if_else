9bb9063e9586cd1f844a60b4e5b5405d4554568c
Andre <meirezende@hotmail.com>
2022-02-10 15:06:07 -0300

    Changed script CHANGELOG path

--- .github/scripts/new_version.sh
+++ .github/scripts/new_version.sh
@@ -1,1 +1,6 @@
-echo $(( ( RANDOM % 10 )  + 1 ))
+ [[ $(grep --count "{version}" './../../CHANGELOG.md') -ne 1 ]] && echo "Placeholder {version} not defined in CHANGELOG.md" && exit 1
+ [[ $(grep --count "{date}" './../../CHANGELOG.md') -ne 1 ]] && echo "Placeholder {date} not defined in CHANGELOG.md" && exit 1
+ [[ $(grep --fixed-strings --count "# $VERSION" './../../CHANGELOG.md') -ne 0 ]] && echo "Version $VERSION already defined in CHANGELOG.md" && exit 1
+
+ sed -i 's/{version}/'"$VERSION"'/' './../../CHANGELOG.md'
+ sed -i 's!{date}!'"$DATE"'!' './../../CHANGELOG.md'
